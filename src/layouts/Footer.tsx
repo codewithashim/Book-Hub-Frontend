17cb8029c7ff0d0a2bf32a9f66a3184339bb685c
Ashim Rudra Paul <codewithashim@gmail.com>
2023-07-18 03:10:22 +0600

    [DONE] Footer is added

--- src/layouts/Footer.tsx
+++ src/layouts/Footer.tsx
@@ -1,40 +1,38 @@
 import logo from "../assets/bookhublogo.png";
-import { RiFacebookBoxFill, RiInstagramLine } from "react-icons/ri";
+
 export default function Footer() {
   const date = new Date();
   const year = date.getFullYear();
 
   return (
-    <div className="bg-[#242630] text-secondary p-20">
-      <div className="flex justify-between">
-        <div>
-          <img className="h-10" src={logo} alt="Logo" />
-        </div>
-        <div className="flex gap-20">
-          <ul className="space-y-2">
-            <li>Upcoming</li>
-            <li>Shipping</li>
-            <li>How it works</li>
-          </ul>
-          <ul className="space-y-2">
-            <li>Support</li>
-            <li>Careers</li>
-          </ul>
-          <ul className="space-y-2">
-            <li>List your gear</li>
-            <li>Contact team</li>
-          </ul>
-        </div>
-        <div className="flex gap-2 text-2xl">
-          <RiFacebookBoxFill />
-          <RiInstagramLine />
-        </div>
+    <footer className="footer p-10 bg-base-200 text-base-content">
+      <div>
+        <img src={logo} alt="BookHub Logo" className="w-40" />
+        <p>
+          BookHub | 1234 Main St | Anywhere, USA | 123-456-7890
+          <br />
+          Providing reliable book service since {year}
+        </p>
       </div>
-      <div className="flex w-full mt-20 gap-5">
-        <p>Privacy Policy</p>
-        <p>Terms & Condition</p>
-        <p className="ml-auto"> &#169; TechNet {year}</p>
+      <div>
+        <span className="footer-title">Services</span>
+        <a className="link link-hover">Branding</a>
+        <a className="link link-hover">Design</a>
+        <a className="link link-hover">Marketing</a>
+        <a className="link link-hover">Advertisement</a>
       </div>
-    </div>
+      <div>
+        <span className="footer-title">Company</span>
+        <a className="link link-hover">All Books</a>
+        <a className="link link-hover">About us</a>
+        <a className="link link-hover">Contact</a>
+      </div>
+      <div>
+        <span className="footer-title">Legal</span>
+        <a className="link link-hover">Terms of use</a>
+        <a className="link link-hover">Privacy policy</a>
+        <a className="link link-hover">Cookie policy</a>
+      </div>
+    </footer>
   );
 }
--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -1,13 +1,15 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "./Navbar";
+import Footer from "./Footer";
 
 export default function MainLayout() {
   return (
     <main>
       <Navbar />
-      <section className="pt-2">
+      <section className="py-2 mb-4">
         <Outlet />
       </section>
+      <Footer />
     </main>
   );
 }
