70c655fe33e0cfc8ccd675bf710c305da2fb5751
Ashim Rudra Paul <codewithashim@gmail.com>
2023-07-29 04:05:04 +0600

    Api Is deployed and connect

--- src/redux/api/apiSlice.ts
+++ src/redux/api/apiSlice.ts
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const api = createApi({
   reducerPath: "api",
   baseQuery: fetchBaseQuery({
-    baseUrl: "http://localhost:8080/api/v1",
+    baseUrl: "https://bookhub-server-omega.vercel.app/api/v1",
   }),
   tagTypes: ["book"],
   endpoints: () => ({}),
